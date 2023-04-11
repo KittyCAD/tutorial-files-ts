9412c23d34043e228300452f328081903efb64d3
jgomez720 <114548659+jgomez720@users.noreply.github.com>
2023-04-11 15:50:40 -0700

    update objtostep.js file

--- convertOBJtoSTEP.js
+++ convertOBJtoSTEP.js
@@ -2,13 +2,16 @@ import { file } from '@kittycad/lib'
 import fsp from 'fs/promises'
 
 async function convertOBJtoSTEP() {
+  // Use KittyCAD client library to output base64 string from OBJ to STEP
     const response = await file.create_file_conversion({
         output_format: 'step',
         src_format: 'obj',
         body: await fsp.readFile('./gear.obj', 'base64'),
     })
-    //const decodedSTEPfile = atob(response.output)
+    // Decode the base64 string
     const decodedSTEPfile = Buffer.from(response.output, "base64").toString("utf-8")
+
+    // Use fs/promises library to write a new STEP file using the decoded base64 string
     fsp.writeFile('gear.step', decodedSTEPfile, function (err) {
       if (err) throw err;
     })
@@ -16,7 +19,6 @@ async function convertOBJtoSTEP() {
     else {
       console.log('File created successfully.');
     }
-    
 }
 
 convertOBJtoSTEP()
